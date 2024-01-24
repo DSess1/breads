39dface18ee70a785f4e87267f0ef8cb36e5071f
user name <user email>
2024-01-24 18:26:28 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ app.set('views', __dirname + '/views')//must add this line in your rest rant fol
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+
+// MIDDLEWARE
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) =>
 {
     res.send('Welcome to an Awesome app about Breads')
 });
 
 app.use('/breads', require('./controllers/breads_controller'))
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
